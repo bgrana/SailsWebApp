ef24ba6ff694e82cd0c37c8d0988a557201bd584
bgrana <bgrana@conwet.com>
2015-06-25 07:21:36 +0200

    Use session.authenticated flag in sessionAuth policy

--- api/policies/sessionAuth.js
+++ api/policies/sessionAuth.js
@@ -9,17 +9,13 @@
  */
 module.exports = function(req, res, next) {
 
-  if (req.user || req.baseUrl === '/') {
-    return next();
+  // User is allowed, proceed to the next policy, 
+  // or if this is the last policy, the controller
+  if (req.session.authenticated) {
+   return next();
   }
 
   res.redirect('/login');
-
-  // User is allowed, proceed to the next policy, 
-  // or if this is the last policy, the controller
-  //if (req.session.authenticated) {
-  //  return next();
-  //}
 
   // User is not allowed
   // (default res.forbidden() behavior can be overridden in `config/403.js`)
