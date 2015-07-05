0316e40cce327c1851faa35e54db11db80c5ad6a
Jaime Pajuelo <jpajuelo@conwet.com>
2015-07-06 01:02:56 +0200

    Add auth controller for users

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -35,6 +35,8 @@ var jsFilesToInject = [
 
   // All of the rest of your client-side js files
   // will be injected here in no particular order.
+  'js/modules/user/module.js',
+  'js/modules/user/controllers/**/*.js',
   'js/**/*.js'
 ];
 
