cd83557d24b05a9ba1bef80e7645e1d2992ec4a8
bgrana <bgrana@conwet.com>
2015-07-06 08:21:49 +0200

    Admin users can now perform operations on other models

--- api/policies/isReviewOwnerOrAdmin.js
+++ api/policies/isReviewOwnerOrAdmin.js
@@ -4,7 +4,7 @@ module.exports = function (req, res, next) {
 
         if (found.length === 0) return res.json(404, "Review not found.");
 
-        if (found[0].owner !== req.user.id) {
+        if (found[0].owner !== req.user.id && req.user.role !== 'admin') {
             return res.forbidden('Only the owner of a review can permform operations on it.');
         }
 
--- api/policies/isUserOrAdmin.js
+++ api/policies/isUserOrAdmin.js
@@ -1,5 +1,5 @@
 module.exports = function (req, res, next) {
-    if (req.user.id !== req.param('userid')) {
+    if (req.user.id !== req.param('userid') && req.user.role !== 'admin') {
         return res.forbidden('You are not allowed to perform operations on other users.');
     }
 
