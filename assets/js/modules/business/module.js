3d690bf454a3c563f04ef91e8ccde819d1210dbd
bgrana <bgrana@conwet.com>
2015-07-08 20:10:14 +0200

    Add map to business profile view

--- assets/js/modules/business/module.js
+++ assets/js/modules/business/module.js
@@ -1,8 +1,8 @@
 (function () {
-    var app = angular.module('BusinessModule', []);
+    var app = angular.module('BusinessModule', ['uiGmapgoogle-maps']);
 
     app.controller('BusinessController', ['$scope', '$http', '$element', '$interpolate', function ($scope, $http, $element, $interpolate) {
-            $scope.step1Completed = false;
+            $scope.step1Completed = true;
             $scope.createModel = {};
             $scope.storeCreateModel = {};
             $scope.step2Completed = false;
@@ -54,6 +54,7 @@
     app.controller('BusinessListController', ['$scope', '$http', '$element', '$interpolate', function ($scope, $http, $element, $interpolate) {
 
         $scope.businessId = $element.attr('data-id');
+        $scope.map = { center: { latitude: 40.4143946, longitude: -3.8444746 }, zoom: 15, draggable: true };
 
         $http.get('/business/' + $scope.businessId)
             .success(function(data) {
@@ -70,6 +71,7 @@
         $scope.showStore = function ($index) {
             $scope.stores.forEach(function (store, index) {
                 store.active = index == $index;
+                $scope.map.center = { latitude: 40.41, longitude: -3.9 };
             });
         };
 
