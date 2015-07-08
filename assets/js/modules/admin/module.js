e24a3e6522dc860ab4dc4548c95ce47dc8764ebc
Ignacio Uyá Lasarte <i.uya.lasarte@gmail.com>
2015-07-08 19:27:05 +0200

    Admin can create and delete users

--- assets/js/modules/admin/module.js
+++ assets/js/modules/admin/module.js
@@ -5,22 +5,45 @@
   app.controller('UserController', [ '$scope', '$http',  function($scope , $http){
     var admin = this;
     admin.users = [];
+    userModel = {};
 
     $http.get('/user').success( function(data){
       admin.users = data;
     });
 
     $scope.updateUser = function (user) {
-      $http.post('/user', user)
+      $http.put('/user/'+user.id, user)
         .success(function () {
           alert('User info updated');
+        }).error(function(err){
+          console.log(err);
         });
     };
+
+    $scope.createUser = function (user) {
+      $http.post('/user', user)
+        .success(function () {
+          alert('New user created');
+        }).error(function(err){
+          console.log(err);
+        });
+    };
+
+    $scope.destroyUser = function (user) {
+      $http.delete('/user'+user.id, user)
+        .success(function () {
+          alert('New user created');
+        }).error(function(err){
+          console.log(err);
+        });
+    };
+
   }]);
 
   app.controller('LocalController', [ '$scope', '$http', function($scope , $http){
     var admin = this;
     admin.locals = [];
+    localModel = {};
 
     $http.get('/local').success( function(data){
       admin.locals = data;
@@ -30,6 +53,8 @@
       $http.post('/local', local)
         .success(function () {
           alert('User info updated');
+        }).error(function(err){
+          console.log(err);
         });
     };
 
@@ -38,6 +63,7 @@
   app.controller('BusinessController', [ '$scope', '$http', function($scope , $http){
     var admin = this;
     admin.businesses = [];
+    businessModel = {};
 
     $http.get('/business').success( function(data){
       admin.businesses = data;
@@ -47,9 +73,10 @@
       $http.post('/local', local)
         .success(function () {
           alert('User info updated');
+        }).error(function(err){
+          console.log(err);
         });
     };
-
   }]);
 
 })();
