ef94c490255a71d125dff65905167ec14f28097c
bgrana <bgrana@conwet.com>
2015-07-09 19:35:08 +0200

    Add map with markers to home page

--- assets/js/modules/user/module.js
+++ assets/js/modules/user/module.js
@@ -40,12 +40,31 @@ angular.module('UserModule', ['AdminModule', 'BusinessModule'])
   .controller('HomeController', ['$scope', '$http', '$element', function ($scope, $http, $element) {
 
       $scope.locals = [];
+      $scope.map = { center: { latitude: 40.416599610694654, longitude: -3.703838038028721 }, zoom: 18, control: {}, markers: [] };
 
       $http.get('/local')
         .success(function (locals) {
           $scope.locals = locals;
+          $scope.locals.forEach(function (local, index) {
+            var marker = {
+              id: index,
+              coords: {
+                latitude: local.location.A,
+                longitude: local.location.F
+              }
+            };
+
+            $scope.map.markers.push(marker);
+          });
+          $scope.map.control.refresh({ latitude: 40.416599610694654, longitude: -3.703838038028721 });
         })
         .error(function (errorMessage) {
         });
+
+      $scope.showLocal = function ($index) {
+            $scope.locals.forEach(function (store, index) {
+                store.active = index == $index;
+            });
+        };
 
   }]);
